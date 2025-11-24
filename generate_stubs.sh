4cf9da8a23492a0d17f1e2f414ac97554d78129a
Ahmed Essam <aessam.dahy@gmail.com>
2025-11-24 22:07:57 +0200

    Use mypy stubgen instead of pybind11-stubgen
    Should I add the stubgens to the git tree? Should I manually maintain
    them? Both pybind11-stubgen and mypy stubgen have their limitations.

--- generate_stubs.sh
+++ generate_stubs.sh
@@ -7,7 +7,8 @@ source "$SCRIPT_DIR/.venv/bin/activate"
 export PYTHONPATH="$SCRIPT_DIR/build/cpp"
 
 cd "$SCRIPT_DIR/build"
-pybind11-stubgen polychase_core --numpy-array-use-type-var &&
+
+stubgen -m polychase_core &&
     mkdir -p "$SCRIPT_DIR/blender_addon/lib" &&
-    cp ./stubs/polychase_core.pyi "$SCRIPT_DIR/blender_addon/lib/polychase_core.pyi" &&
-    rm ./stubs -rf
+    cp ./out/polychase_core.pyi "$SCRIPT_DIR/blender_addon/lib/polychase_core.pyi" &&
+    rm ./out -rf
